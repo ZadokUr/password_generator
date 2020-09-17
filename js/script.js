92d9fdb3551121df9a9ea5edb6a261dbbff38be6
unknown <Alinanisichone@outlook.com>
2020-09-17 11:28:33 +0800

    Completed Logic. Small bug remains

--- js/script.js
+++ js/script.js
@@ -5,10 +5,44 @@ output.innerHTML = slider.value;
 slider.oninput = function () {
   output.innerHTML = this.value;
 }
+
+
+var password = document.getElementById("password");
+var copyButton = document.getElementById("copyButton");
+var resetButton = document.getElementById("resetButton");
+var uppercase = document.getElementById("uppercase");
+var lowercase = document.getElementById("lowercase");
+var numbers = document.getElementById("numbers");
+var symbols = document.getElementById("symbols");
+
+var generateButton = document.getElementById("generateButton");
+
+generateButton.onclick = ()=>{
+  if(uppercase.checked && lowercase.checked && numbers.checked && !symbols.checked) password.innerHTML = generatePasswordLowerUpperNumber(slider.value);
+  else if (uppercase.checked && lowercase.checked && symbols.checked && !numbers.checked) password.innerHTML = generatePasswordLowerUpperSymbol(slider.value);
+  else if(uppercase.checked && numbers.checked && symbols.checked && !lowercase.checked) password.innerHTML = generatePasswordUpperNumberSymbol(slider.value);
+  else if(lowercase.checked && numbers.checked && symbols.checked && !uppercase.checked) password.innerHTML = generatePasswordLowerNumberSymbol(slider.value);
+  else if(uppercase.checked && numbers.checked && !lowercase.checked && !symbols.checked) password.innerHTML = generatePasswordUpperNumber(slider.value);
+  else if(lowercase.checked && numbers.checked && !uppercase.checked && !symbols.checked) password.innerHTML = generatePasswordLowerNumber(slider.value);
+  else if(uppercase.checked && symbols.checked && !lowercase.checked && !numbers.checked)  password.innerHTML = generatePasswordUpperSymbol(slider.value);
+  else if(lowercase.checked && symbols.checked && !uppercase.checked && !numbers.checked) password.innerHTML = generatePasswordLowerSymbol(slider.value);
+  else if(numbers.checked && symbols.checked && !lowercase.checked && !uppercase.checked) password.innerHTML = generatePasswordNumberSymbol(slider.value);
+  else if(uppercase.checked && lowercase.checked && !numbers.checked && !symbols.checked) password.innerHTML = generatePasswordLowerUpper(slider.value);
+  else if(uppercase.checked && !lowercase.checked && !numbers.checked && !symbols.checked) password.innerHTML = generatePasswordUpper(slider.value);
+  else if(lowercase.checked && !uppercase.checked && !numbers.checked && !symbols.checked) password.innerHTML = generatePasswordLower(slider.value);
+  else if(numbers.checked && !lowercase.checked && !uppercase.checked && !symbols.checked) password.innerHTML = generatePasswordNumber(slider.value);
+  else if(symbols.checked && !lowercase.checked && !numbers.checked && !uppercase.checked) password.innerHTML = generatePasswordSymbol(slider.value);
+  else  password.innerHTML = generatePassword(slider.value);
+}
+
+resetButton.onclick =()=>{
+  generateButton.click();
+}
+
 //generate an all-lowerase password
 function generatePasswordLower(len) {
   var str = "";
-  for (var i = 0; i < len; i++) { // Loop `len` times
+  while (str.length <= len) { // Loop `len` times
     var rand = Math.floor(Math.random() * (26) + 97);
     str += String.fromCharCode(rand);
   }
@@ -18,7 +52,7 @@ function generatePasswordLower(len) {
 //generate an all-uppercase password
 function generatePasswordUpper(len) {
   var str = "";
-  for (var i = 0; i < len; i++) { // Loop `len` times
+  while (str.length <= len) { // Loop `len` times
     var rand = Math.floor(Math.random() * (26) + 65);
     str += String.fromCharCode(rand);
   }
@@ -27,7 +61,7 @@ function generatePasswordUpper(len) {
 //generate an all-numeric password
 function generatePasswordNumber(len) {
   var str = "";
-  for (var i = 0; i < len; i++) { // Loop `len` times
+  while (str.length <= len) { // Loop `len` times
     var rand = Math.floor(Math.random() * (10) + 48);
     str += String.fromCharCode(rand);
   }
@@ -38,7 +72,7 @@ function generatePasswordNumber(len) {
 // a number between 0-4 is randomly selected, then a symbol from that particular range (as indexed above), is selected
 function generatePasswordSymbol(len) {
   var str = "";
-  for (var i = 0; i < len; i++) {
+  while (str.length <= len) {
     var randRange = Math.floor(Math.random() * 4);
     if (randRange === 0) {
       var rand = Math.floor(Math.random() * (15) + 33)
@@ -59,7 +93,7 @@ function generatePasswordSymbol(len) {
 //generates a password containing lowercase, uppercase and symbols
 function generatePasswordLowerUpperSymbol(len) {
   var str = "";
-  for (var i = 0; i < len; i++) {
+  while (str.length <= len) {
     var randRange = Math.floor(Math.random() * 6);
     if (randRange === 0) {
       var rand = Math.floor(Math.random() * (15) + 33)
@@ -73,10 +107,10 @@ function generatePasswordLowerUpperSymbol(len) {
     } else if (randRange === 3) {
       var rand = Math.floor(Math.random() * (4) + 123)
       str += String.fromCharCode(rand)
-    } else if(randRange === 4){
-    var rand = Math.floor(Math.random() * (26) + 65);
-    str += String.fromCharCode(rand);
-    } else{
+    } else if (randRange === 4) {
+      var rand = Math.floor(Math.random() * (26) + 65);
+      str += String.fromCharCode(rand);
+    } else {
       var rand = Math.floor(Math.random() * (26) + 97);
       str += String.fromCharCode(rand);
     }
@@ -87,7 +121,7 @@ function generatePasswordLowerUpperSymbol(len) {
 //generate password made of upper-case and symbols
 function generatePasswordUpperSymbol(len) {
   var str = "";
-  for (var i = 0; i < len; i++) {
+  while (str.length <= len) {
     var randRange = Math.floor(Math.random() * 5);
     if (randRange === 0) {
       var rand = Math.floor(Math.random() * (15) + 33)
@@ -98,10 +132,10 @@ function generatePasswordUpperSymbol(len) {
     } else if (randRange === 2) {
       var rand = Math.floor(Math.random() * (6) + 91)
       str += String.fromCharCode(rand)
-    } else if (randRange === 3){
+    } else if (randRange === 3) {
       var rand = Math.floor(Math.random() * (4) + 123)
       str += String.fromCharCode(rand)
-    } else{
+    } else {
       var rand = Math.floor(Math.random() * (26) + 65);
       str += String.fromCharCode(rand);
     }
@@ -112,7 +146,7 @@ function generatePasswordUpperSymbol(len) {
 //generate password made of lower-case and symbols
 function generatePasswordLowerSymbol(len) {
   var str = "";
-  for (var i = 0; i < len; i++) {
+  while (str.length <= len) {
     var randRange = Math.floor(Math.random() * 5);
     if (randRange === 0) {
       var rand = Math.floor(Math.random() * (15) + 33)
@@ -123,12 +157,12 @@ function generatePasswordLowerSymbol(len) {
     } else if (randRange === 2) {
       var rand = Math.floor(Math.random() * (6) + 91)
       str += String.fromCharCode(rand)
-    } else if (randRange === 3){
+    } else if (randRange === 3) {
       var rand = Math.floor(Math.random() * (4) + 123)
       str += String.fromCharCode(rand)
-    } else{
+    } else {
       var rand = Math.floor(Math.random() * (26) + 97);
-    str += String.fromCharCode(rand);
+      str += String.fromCharCode(rand);
     }
   }
   return str;
@@ -136,7 +170,7 @@ function generatePasswordLowerSymbol(len) {
 //generate password made of numbers and symbols
 function generatePasswordNumberSymbol(len) {
   var str = "";
-  for (var i = 0; i < len; i++) {
+  while (str.length <= len) {
     var randRange = Math.floor(Math.random() * 5);
     if (randRange === 0) {
       var rand = Math.floor(Math.random() * (15) + 33)
@@ -147,12 +181,12 @@ function generatePasswordNumberSymbol(len) {
     } else if (randRange === 2) {
       var rand = Math.floor(Math.random() * (6) + 91)
       str += String.fromCharCode(rand)
-    } else if (randRange === 3){
+    } else if (randRange === 3) {
       var rand = Math.floor(Math.random() * (4) + 123)
       str += String.fromCharCode(rand)
-    } else{
+    } else {
       var rand = Math.floor(Math.random() * (10) + 48);
-    str += String.fromCharCode(rand);
+      str += String.fromCharCode(rand);
     }
   }
   return str;
@@ -160,7 +194,7 @@ function generatePasswordNumberSymbol(len) {
 //generate password made of numbers and symbols and lowercase
 function generatePasswordLowerNumberSymbol(len) {
   var str = "";
-  for (var i = 0; i < len; i++) {
+  while (str.length <= len) {
     var randRange = Math.floor(Math.random() * 6);
     if (randRange === 0) {
       var rand = Math.floor(Math.random() * (15) + 33)
@@ -171,15 +205,15 @@ function generatePasswordLowerNumberSymbol(len) {
     } else if (randRange === 2) {
       var rand = Math.floor(Math.random() * (6) + 91)
       str += String.fromCharCode(rand)
-    } else if (randRange === 3){
+    } else if (randRange === 3) {
       var rand = Math.floor(Math.random() * (4) + 123)
       str += String.fromCharCode(rand)
-    } else if (rand===4){
+    } else if (rand === 4) {
       var rand = Math.floor(Math.random() * (26) + 65);
       str += String.fromCharCode(rand);
-    } else{
+    } else {
       var rand = Math.floor(Math.random() * (10) + 48);
-    str += String.fromCharCode(rand);
+      str += String.fromCharCode(rand);
     }
   }
   return str;
@@ -187,7 +221,7 @@ function generatePasswordLowerNumberSymbol(len) {
 //generate password made of numbers and symbols and uppercase
 function generatePasswordUpperNumberSymbol(len) {
   var str = "";
-  for (var i = 0; i < len; i++) {
+  while (str.length <= len) {
     var randRange = Math.floor(Math.random() * 6);
     if (randRange === 0) {
       var rand = Math.floor(Math.random() * (15) + 33)
@@ -198,15 +232,15 @@ function generatePasswordUpperNumberSymbol(len) {
     } else if (randRange === 2) {
       var rand = Math.floor(Math.random() * (6) + 91)
       str += String.fromCharCode(rand)
-    } else if (randRange === 3){
+    } else if (randRange === 3) {
       var rand = Math.floor(Math.random() * (4) + 123)
       str += String.fromCharCode(rand)
-    } else if (rand===4){
+    } else if (rand === 4) {
       var rand = Math.floor(Math.random() * (26) + 97);
       str += String.fromCharCode(rand);
-    } else{
+    } else {
       var rand = Math.floor(Math.random() * (10) + 48);
-    str += String.fromCharCode(rand);
+      str += String.fromCharCode(rand);
     }
   }
   return str;
@@ -215,7 +249,7 @@ function generatePasswordUpperNumberSymbol(len) {
 //generate password made of upper and lowercase letters
 function generatePasswordLowerUpper(len) {
   var str = "";
-  for (var i = 0; i < len; i++) {
+  while (str.length <= len) {
     var randRange = Math.floor(Math.random() * 2);
     if (randRange === 0) {
       var rand = Math.floor(Math.random() * (26) + 65)
@@ -223,31 +257,32 @@ function generatePasswordLowerUpper(len) {
     } else {
       var rand = Math.floor(Math.random() * (26) + 97)
       str += String.fromCharCode(rand)
+    }
   }
+  return str;
 }
-return str;
-}
+
 function generatePasswordLowerUpperNumber(len) {
   var str = "";
-  for (var i = 0; i < len; i++) {
+  while (str.length <= len) {
     var randRange = Math.floor(Math.random() * 3);
     if (randRange === 0) {
       var rand = Math.floor(Math.random() * (26) + 65)
       str += String.fromCharCode(rand)
-    } else if (randRange ===1){
+    } else if (randRange === 1) {
       var rand = Math.floor(Math.random() * (26) + 97)
       str += String.fromCharCode(rand)
-  } else{
-    var rand = Math.floor(Math.random() * (10) + 48);
-    str += String.fromCharCode(rand);
+    } else {
+      var rand = Math.floor(Math.random() * (10) + 48);
+      str += String.fromCharCode(rand);
+    }
   }
-}
-return str;
+  return str;
 }
 //generate password of lowercase letters and numbers
 function generatePasswordLowerNumber(len) {
   var str = "";
-  for (var i = 0; i < len; i++) {
+  while (str.length <= len) {
     var randRange = Math.floor(Math.random() * 2);
     if (randRange === 0) {
       var rand = Math.floor(Math.random() * (10) + 48)
@@ -255,14 +290,14 @@ function generatePasswordLowerNumber(len) {
     } else {
       var rand = Math.floor(Math.random() * (26) + 97)
       str += String.fromCharCode(rand)
+    }
   }
-}
-return str;
+  return str;
 }
 //generate password of uppercase letters and numbers
 function generatePasswordUpperNumber(len) {
   var str = "";
-  for (var i = 0; i < len; i++) {
+  while (str.length <= len) {
     var randRange = Math.floor(Math.random() * 2);
     if (randRange === 0) {
       var rand = Math.floor(Math.random() * (26) + 65)
@@ -270,14 +305,15 @@ function generatePasswordUpperNumber(len) {
     } else {
       var rand = Math.floor(Math.random() * (10) + 48)
       str += String.fromCharCode(rand)
+    }
   }
+  return str;
 }
-return str;
-}
+
 //generate password with all parameters
 function generatePassword(len) {
   var str = "";
-  for (var i = 0; i < len; i++) { // Loop `len` times
+  while (str.length <= len) { // Loop `len` times
     var rand = Math.floor(Math.random() * (94) + 33);
     str += String.fromCharCode(rand);
   }
